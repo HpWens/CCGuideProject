7e6dfadf31d46dd1e247c4ac33a7bc59219bd286
HpWens <373872690@qq.com>
2020-11-06 14:11:24 +0800

    [NEW]初始化

--- app/src/main/java/com/mei/guide/load/DataConstants.java
+++
@@ -1,7 +0,0 @@
-package com.mei.guide.load;
-
-public class DataConstants {
-    public static final String URL = "url";
-    public static final String WHITE_URL = "whiteUrlList";
-    public static final String TITLE = "title";
-}
